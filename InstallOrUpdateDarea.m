1662820317c2c121e7bd245af2fe03af61fd3b94
David Kleindienst <david.kleindienst@ist.ac.at>
2021-12-20 13:52:55 +0100

    Fixed issues with install script on Mac

--- InstallOrUpdateDarea.m
+++ InstallOrUpdateDarea.m
@@ -9,25 +9,32 @@ if isfile('Run.m')
 
     if contains(r, 'https://github.com/DavidKleindienst/Darea.git')
         !git pull origin master
+        fprintf('Updated Darea.')
     elseif contains(r, 'fatal: not a git repository')
-        sprintf(['Darea was not installed using this script and can thus not be updated automatically.\n' ...
+        fprintf(['Darea was not installed using this script and can thus not be updated automatically.\n' ...
                 'Please copy this script to an empty folder and run it there to install Darea in a way' ...
-                'that supports automatic updating']);
-    elseif contains(r, 'is not recognized as an internal or external command')
-        sprintf('Git is not installed. Please install git, restart MATLAB then run this script again')
+                'that supports automatic updating\n']);
+    elseif contains(r, 'is not recognized as an internal or external command\n')
+        fprintf('Git is not installed. Please install git, restart MATLAB then run this script again\n')
     else
-        sprintf('Unknown github repository, could not update');
+        fprintf('Unknown github repository, could not update\n');
     end
 else
     [~,r]=system('git clone https://github.com/DavidKleindienst/Darea.git');
     if contains(r, 'Cloning into')
         movefile('Darea/*', './');
+        if isfile('Darea/.gitignore')
+            % On MacOS, movefile doesn't move hidden files unless explicitly asked
+            movefile('Darea/.g*', './')
+        end
+        isfile('Darea/.gitignore')
+        isfile('.gitignore')
         rmdir('Darea');
-        sprintf('Installation was successful');
-    elseif contains(r, 'is not recognized as an internal or external command')
-        sprintf('Git is not installed. Please install git, restart MATLAB and try again')
+        fprintf('Installation was successful\n');
+    elseif contains(r, 'is not recognized as an internal or external command\n')
+        fprintf('Git is not installed. Please install git, restart MATLAB and try again\n')
     else
-        sprintf('An unknown problem occur. Please make sure git is installed, restart MATLAB and try again');
+        fprintf('An unknown problem occur. Please make sure git is installed, restart MATLAB and try again\n');
     end
         
 end
