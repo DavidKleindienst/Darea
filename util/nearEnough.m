c91314599023b0841c6624ed1daaa4254f6373b1
David Kleindienst <dkleindienst@ist.ac.at>
2021-12-21 14:37:29 +0100

    Can now deal with arrays

--- util/nearEnough.m
+++ util/nearEnough.m
@@ -1,23 +1,21 @@
-function out = nearEnough(in1,in2)
+function out = nearEnough(in1,in2,maxDifference)
 %% Compares equality of two numbers, returns true or false.
-% Returns true if numbers are virtually identically i.e. difference between them is less than 10^6
+% Returns true if numbers are virtually identically i.e. difference between
+% them is less max Difference (default 10^-6)
 % This is to avoid false non-equality due to rounding errors
 
-if isnan(in1) && isnan(in2)
-    out=true;
-elseif isnan(in1) || isnan(in2)
-    out=false;
-elseif isa(in1,'double') || isa(in2,'double')
-    if in1-in2<0.000001 && in1-in2>-0.000001
-        out=true;
-    else
-        out=false;
-    end
-else
-    if in1==in2
-        out=true;
-    else
-        out=false;
-    end
+if nargin<3
+    maxDifference = 1e-6;
 end
 
+if isnan(in1) & isnan(in2)
+    out=true;
+elseif isnan(in1) | isnan(in2)
+    out=false;
+elseif isa(in1,'double') | isa(in2,'double')
+    out = (in1-in2)<maxDifference & (in1-in2)>-maxDifference;
+else
+    out = in1==in2;
+end
+
+end
