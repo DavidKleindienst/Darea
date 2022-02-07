fd60d651372af228ef76ff469b49b991ccbebb01
David Kleindienst <david.kleindienst@ist.ac.at>
2022-02-07 11:50:03 +0100

    Experimental implementation for proper distance from center/edge when multiple demarcations are present. Still needs some work and is not yet used.

--- processing/util/distanceFromCenter.m
+++ processing/util/distanceFromCenter.m
@@ -5,12 +5,27 @@ if size(points,1) <1
     distance = NaN;
     return
 end
+if iscell(boundary)
+    %get list of centroids c
+    % distance = min(pdist2(points,centroids),[],2))
+    distances = zeros(numel(points),1);
+    for b = 1:numel(boundary)
+        polygon=polyshape(boundary{b}(:,1), boundary{b}(:,2));
+        % ToDo: Decide whether this boundary sourrounds a demarcated region
+        % If Donut shaped demarcated region, only outer boundary should be further processed
+        [x, y]=centroid(polygon);
+        distance=pdist2(points,[x,y]);
+        pointsContained=isinterior(polygon,points(:,1), points(:,2));
+        distances(pointsContained) = distance(pointsContained);
+    end
+else
+    %Compute center of gravity
+    polygon=polyshape(boundary(:,1), boundary(:,2));
+    [x, y]=centroid(polygon);
 
-%Compute center of gravity
-polygon=polyshape(boundary(:,1), boundary(:,2));
-[x, y]=centroid(polygon);
+    distance=pdist2(points,[x,y]);
+end
 
-distance=pdist2(points,[x,y]);
 
 end
 
--- processing/util/distanceFromEdge.m
+++ processing/util/distanceFromEdge.m
@@ -6,12 +6,30 @@ if size(points,1) <1
     distanceEdge = NaN;
     return
 end
-try
-    distanceEdge=p_poly_dist(points(:,1),points(:,2),boundary(:,1),boundary(:,2));
-catch
-    points
-    boundary
-    distanceEdge=p_poly_dist(points(:,1),points(:,2),boundary(:,1),boundary(:,2));
+if iscell(boundary)
+    distances = [];
+    for b = 1:numel(boundary)
+        try
+            distances = [distances, p_poly_dist(points(:,1),points(:,2),boundary{b}(:,1),boundary{b}(:,2))];
+        catch e
+            points
+            boundary
+            b
+            rethrow(e);
+        end
+        
+        distanceEdge = max(distances,[],2);
+    end
+    
+else
+    try
+        distanceEdge=p_poly_dist(points(:,1),points(:,2),boundary(:,1),boundary(:,2));
+    catch e
+        points
+        boundary
+        rethrow(e);
+    end
 end
-distanceEdge=distanceEdge*-1;
+distanceEdge = distanceEdge*-1;
+
 end
